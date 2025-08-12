0fa48b59195faf1fcd84beadf991695854a37a2e
ssjjss1 <maetan.s210094@ggh.goe.go.kr>
2025-08-13 01:31:32 +0900

    lv2

--- Lv2부터 치트였던 전직 아이돌 한별이의 알록달록 트리 라이프.cpp
+++ Lv2부터 치트였던 전직 아이돌 한별이의 알록달록 트리 라이프.cpp
@@ -1,28 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define INF 300004
 using namespace std;
+typedef long long ll;
 
-vector<long long> adj[INF];
-vector<vector<long long>> dp(INF, vector<long long>(4, 0));
+vector<int> adj[INF];
+vector<vector<ll>> dp(INF, vector<ll>(4, 0));
+int n;
+ll m;
 
-long long dfs(int node, int p, int gp){
-    
+void dfs(int cx, int p) {
+    vector<ll> res(4);
+    res[0] = 1;
+    res[1] = 0;
+    res[2] = 1;
+    res[3] = 0;
+
+    for (auto nx : adj[cx]) {
+        if (nx == p) continue;
+        dfs(nx, cx);
+        
+        vector<ll> temp(4);
+        temp[0] = res[0] * ((dp[nx][0] + dp[nx][1]) % m) % m;
+        temp[1] = (res[1] * ((dp[nx][0] + dp[nx][1]) % m) + res[0] * dp[nx][2]) % m;
+        temp[2] = res[2] * ((dp[nx][2] + dp[nx][3]) % m) % m;
+        temp[3] = (res[3] * ((dp[nx][2] + dp[nx][3]) % m) + res[2] * dp[nx][0]) % m;
+
+        res = temp;
+    }
+
+    dp[cx][0] = res[0] % m;
+    dp[cx][1] = res[1] % m;
+    dp[cx][2] = res[2] % m;
+    dp[cx][3] = res[3] % m;
 }
 
-int main(){
+ll mPow(ll a, ll b, ll mod) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    long long n,m;
-    cin>>n>>m;
-
-    for(int i=1;i<=n;i++){
-        long long u,v;
-        cin>>u>>v;
+    cin >> n >> m;
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
-    long long re = dfs(1, -1, -1);
+    dfs(1, -1);
 
+    ll total = mPow(2, n, m);
+    ll re = (dp[1][0] + dp[1][1] + dp[1][2] + dp[1][3]) % m;
+    ll ans = (total - re + m) % m;
+
+    cout << ans;
 }
